b335e5b4f00ca2df60661789f0ded909c53bac71
Josh Toy <toyjtoy@gmail.com>
2015-01-23 21:43:42 +0800

    done with devise views

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,52 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    respond_with(@photo)
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def edit
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to @photo, notice: 'Photo was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @photo.update(photo_params)
+      redirect_to @photo, notice: 'Photo was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to photos_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_photo
+      @photo = Photo.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def photo_params
+      params.require(:photo).permit(:description)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123063419) do
+ActiveRecord::Schema.define(version: 20150123103843) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20150123063419) do
 
   add_index "installs", ["email"], name: "index_installs_on_email", unique: true
   add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
