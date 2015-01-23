cf12168e8c15dea5185f3643f5b83ec6cf45c844
Josh Toy <toyjtoy@gmail.com>
2015-01-23 15:40:54 +0800

    setup devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku --> Set to actual hostname
+  config.action_mailer.default_url_options = { :host => 'balern-commits.herokuapp.com' }
 end
