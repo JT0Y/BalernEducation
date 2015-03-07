a3a7fdf127682ccbda6d967506d940b5abfdc18f
Josh Toy <toyjtoy@gmail.com>
2015-03-07 11:02:25 +0800

    Playing with application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require chosen-jquery
 //= require messages
 //= require turbolinks
-//= require jquery.turbolinks
-
