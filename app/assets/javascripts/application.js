b5affc7ac12af7c7fea8d68166244c6326ae5d09
Josh Toy <toyjtoy@gmail.com>
2015-03-16 15:27:00 +0800

    Added translations. moved js files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
+//= require bootstrap
 //= require jquery.turbolinks
 //= require chosen-jquery
 //= require jquery.image-select
