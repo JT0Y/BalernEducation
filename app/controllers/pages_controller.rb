301ee344881074355e635c82eb65e9b0e681aec7
Josh Toy <toyjtoy@gmail.com>
2015-06-29 17:25:49 +0800

    Attempt to fix bg

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
   def home
   	@resources = Resource.all
-  	@events = Event.order(start_time: :desc)
+  	@events = Event.where('start_time >= ?', Time.now.beginning_of_day)
   	@homeworks = Homework.all
   end
 
